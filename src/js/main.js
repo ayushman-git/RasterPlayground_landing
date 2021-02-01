332de7034001ec52fc5a739b732e1ff087861b9d
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-02-01 18:21:36 +0530

    Added loading animation

--- src/js/main.js
+++ src/js/main.js
@@ -49,3 +49,57 @@ document.addEventListener("scroll", () => {
     menu.style.display = "none";
   }
 });
+
+//animation
+
+const tl = gsap.timeline();
+const tlImg = gsap.timeline();
+const tlShuttle = gsap.timeline({ repeat: -1, yoyo: true });
+
+tl.from(".hero-heading", {
+  duration: 1,
+  y: -20,
+});
+tl.from(
+  ".hero-tagline",
+  {
+    duration: 1,
+    y: -20,
+  },
+  "-=1.3"
+);
+tl.from(
+  "#get-started-btn",
+  {
+    duration: 1,
+    y: -20,
+  },
+  "-=1.8"
+);
+
+tlImg.from(".hero-img", {
+  duration: 3,
+  x: 80,
+  opacity: 0,
+  scale: 1.1,
+});
+
+tlImg.to(".hero-img", {
+  transformOrigin: "50% 50%",
+  duration: 10,
+  scale: 1.05,
+  y: "random(5,15)",
+  repeat: -1,
+  yoyo: true,
+});
+
+tlShuttle.to(".shuttle", {
+  transformOrigin: "50% 50%",
+  duration: 3,
+  rotation: 2,
+});
+tlShuttle.to(".shuttle", {
+  transformOrigin: "50% 50%",
+  duration: 3,
+  rotation: -2,
+});
